21628e8d8a101ac329ec827e5d9ca6dcdbd67107
Rayan84 <rayan_84@live.com>
2021-11-04 09:56:44 +0100

    Fix a bug

--- solution.js
+++ solution.js
@@ -11,6 +11,9 @@ for(let i = 0; i < sortedArray.length; i++){
     let result = Math.abs(sortedArray[startNumber] - sortedArray[i]);
     if (result <= 1){
         currentArray.push(sortedArray[i]);
+        if(currentArray.length > longestArray){
+                longestArray = currentArray.length
+            };
     }else {
 
         startNumber = i;
